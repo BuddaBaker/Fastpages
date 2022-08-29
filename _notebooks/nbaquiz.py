b8c05d317d1af29d49aa648b40b306f5057bd13d
BuddaBaker <prasithchilla@gmail.com>
2022-08-28 22:13:57 -0700

    Edited Quiz removing Errors

--- _notebooks/nbaquiz.py
+++ _notebooks/nbaquiz.py
@@ -1,30 +1,30 @@
 points = 0
-answers = input("Who won the 2021-2022 NBA Championship?")
+answer = input("Who won the 2021-2022 NBA Championship?")
 if answer == "Warriors":
     print("Correct answer!")
     points += 1
 else: 
     print("incorrect answer")
     
-answers = input("Which NBA franchise has the most MVP winners?")
+answer = input("Which NBA franchise has the most MVP winners?")
 if answer == "Celtics":
     print("Correct answer!")
     points += 1
 else: 
     print("incorrect answer")
     
-answers = input("Who won the 2021-2022 NBA MVP?")
+answer = input("Who won the 2021-2022 NBA MVP?")
 if answer == "Nikola Jokic":
     print("Correct answer!")
     points += 1
 else: 
     print("incorrect answer")
 
-answers = input("Which NBA team(s) has the most rings?")
-if answer == "Nikola Jokic":
+answer = input("Which NBA team(s) has the most rings?")
+if answer == "Celtics":
     print("Correct answer!")
     points += 1
 else: 
     print("incorrect answer")
 
-printL("You got:", points/4, "%")
+print("You got:", (points/4)*100, "%")
